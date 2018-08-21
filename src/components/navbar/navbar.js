6a31516256e1f727afc74440f4a1a3a1ca89b0da
victor.marques <victor.marques@mjv.com.br>
2018-08-20 23:35:44 -0300

    mobile menu feature setting

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,3 +4,10 @@ $(function() {
         $('.brand, .shop, .information, .search').fadeToggle(0);
     });
 })
+
+$(window).on('resize', function () {
+    var win = $(this);
+    if (win.width() >= 992) {
+        $('.brand, .shop, .information, .search').show();
+    }
+});
